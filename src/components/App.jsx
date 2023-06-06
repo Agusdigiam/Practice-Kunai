6552a7ee5e23925f94c776f0839ff0ab25116846
Agustin Di Giambatista <agustindigiambatista@gmail.com>
2023-06-06 17:58:01 -0300

    Practice

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+ 
+
+function App() {
+  return (
+    <div>
+        <h1 className='Titulo'>Kunai Practice</h1>
+        <ul className='Navegacion'>
+            <li><a href="">Noticias</a></li>
+            <li><a href="">Media</a></li>
+            <li><a href="">Contacto</a></li>
+        </ul>
+    </div>
+  )
+}
+
+function Click() {
+    alert("Hola Funciono")
+    
+}
+
+export default App
